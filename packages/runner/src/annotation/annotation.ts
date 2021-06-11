37637689884ec349d52f909ce82e97ebb522c991
johns10 <johns10davenport@gmail.com>
2021-06-11 07:26:17 -0400

    Make adjustments to locator location

--- packages/runner/src/annotation/annotation.ts
+++ packages/runner/src/annotation/annotation.ts
@@ -21,16 +21,49 @@ export const annotationHandlers: AnnotationHandler = {
     const annotationId = step.annotation.id;
 
     var locatorElement = document.createElement('div');
+
+    // Append locator to parent
+    elementToAnnotate.prepend(locatorElement);
+
     locatorElement.id = `userdocs-annotation-${annotationId}-locator`
     locatorElement.classList.add("userdocs-locator")
     const styleLocator = new Function(`return ${style.styleLocator}`)()
     locatorElement = styleLocator(locatorElement, elementToAnnotate)
+
+    // Position Locator
+    const parentRect = elementToAnnotate.getBoundingClientRect();
+    const elementRect = locatorElement.getBoundingClientRect();
+    const parentStyle = window.getComputedStyle(elementToAnnotate)
+    const locatorStyle = window.getComputedStyle(locatorElement)
+    const topAdjustment = (parentRect.top - parseInt(parentStyle.marginTop) - parseInt(parentStyle.paddingTop)) - elementRect.top
+    const leftAdjustment = (parentRect.left - parseInt(parentStyle.marginLeft) - parseInt(parentStyle.paddingLeft)) - elementRect.left
+    console.log(`Top adjustment of ${topAdjustment}`)
+    console.log(`
+      Locator Top is ${elementRect.top}. 
+      Parent Top is ${parentRect.top}. 
+      Margin Top is ${parentStyle.marginTop}.  
+      Padding top is ${parseInt(parentStyle.paddingTop)}. 
+      Top adjustment of ${topAdjustment}
+    `)
+    console.log(`
+      Locator Left is ${locatorStyle.left}. 
+      Parent Left is ${parentRect.left}. 
+      Margin Left is ${parentStyle.marginLeft}.  
+      Padding left is ${parseInt(parentStyle.paddingLeft)}. 
+      Left adjustment of ${leftAdjustment}
+    `)
+    locatorElement.style.top = `${parseInt(locatorStyle.top) + topAdjustment}px`
+    locatorElement.style.left = `${parseInt(locatorStyle.left) - leftAdjustment}px`
+    // End Position Locator
 
     var maskElement = document.createElement('div');
     maskElement.id = `userdocs-mask-${annotationId}-mask`
     maskElement.classList.add("userdocs-mask")
     const styleMask = new Function(`return ${style.styleMask}`)()
     maskElement = styleMask(maskElement, elementToAnnotate)
+
+    // Append mask to locator
+    locatorElement.append(maskElement);
   
     var badgeElement = document.createElement('span');
     badgeElement.id = `userdocs-annotation-${annotationId}-badge`
@@ -40,9 +73,8 @@ export const annotationHandlers: AnnotationHandler = {
     badgeElement = styleBadge(badgeElement, size, fontSize, color, xOrientation, yOrientation);
   
     try {
-      elementToAnnotate.prepend(locatorElement);
-      locatorElement.append(maskElement);
       maskElement.append(badgeElement); 
+      badgeElement.style.zIndex = '999'
       if (window.active_annotations) {
         window.active_annotations.push(locatorElement);
       } else {
@@ -59,20 +91,33 @@ export const annotationHandlers: AnnotationHandler = {
     const thickness = step.annotation.thickness;
 
     var locatorElement = document.createElement('div');
+
+
     locatorElement.id = `userdocs-annotation-${annotationId}-locator`
     locatorElement.classList.add("userdocs-locator")
+    elementToAnnotate.prepend(locatorElement);
+
     const styleLocator = new Function(`return ${style.styleLocator}`)()
     locatorElement = styleLocator(locatorElement, elementToAnnotate)
+
+    const parentRect = elementToAnnotate.getBoundingClientRect();
+    const elementRect = locatorElement.getBoundingClientRect();
+    const topAdjustment = parentRect.top - elementRect.top
+    const leftAdjustment = parentRect.left - elementRect.left
+    console.log(`Top adjustment of ${topAdjustment}`)
+    console.log(`Left adjustment of ${leftAdjustment}`)
+    locatorElement.style.top = `${parseInt(locatorElement.style.top) + topAdjustment}px`
+    locatorElement.style.left = `${parseInt(locatorElement.style.left) + leftAdjustment}px`
 
     var outlineElement = document.createElement('div');
     outlineElement.id = `userdocs-annotation-${annotationId}-outline`
     outlineElement.classList.add("userdocs-outline")
+    locatorElement.append(outlineElement)
+
     const styleOutline = new Function(`return ${style.styleOutline}`)()
     outlineElement = styleOutline(elementToAnnotate, outlineElement, color, thickness)
   
     try {
-      elementToAnnotate.append(locatorElement);
-      locatorElement.append(outlineElement)
       if (window.active_annotations) {
         window.active_annotations.push(outlineElement);
       } else {
