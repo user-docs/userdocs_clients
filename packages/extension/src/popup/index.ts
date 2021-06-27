d4fd95f27cfc996a4cca122ec51aa9d557b70c88
johns10 <johns10davenport@gmail.com>
2021-06-27 10:55:54 -0400

    Refactor/makework

--- packages/extension/src/popup/index.ts
+++ packages/extension/src/popup/index.ts
@@ -1,11 +1,13 @@
-import 'alpinejs'
+import { actions } from '../actions'
 
 document.addEventListener('DOMContentLoaded', function () {
-  var btn = document.getElementById('start-authoring');
-  btn.addEventListener('click', function() {
-    console.log('Popup got event, sending to ?content script?')
-    chrome.runtime.sendMessage('startAuthoring', (response) => {
-      console.log(response)
-    })
-  });
+  chrome.storage.local.get([ 'authoring' ], (result) => {
+    var btn: any = document.getElementById('authoring-toggle'); // Bogus, should be HTMLInputElement
+    btn.checked = result.authoring
+    btn.addEventListener('change', function(event) {
+      let target: any = event.target
+      if (target.checked === true) chrome.runtime.sendMessage({ action: actions.START })
+      else chrome.runtime.sendMessage({ action: actions.STOP })
+    });
+  })
 });
