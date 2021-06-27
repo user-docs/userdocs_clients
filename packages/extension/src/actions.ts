3c741d4b55e78df7bbbe98f984cbf6c13ea1082f
johns10 <johns10davenport@gmail.com>
2021-06-27 11:01:39 -0400

    Add additional actions

--- packages/extension/src/actions.ts
+++ packages/extension/src/actions.ts
@@ -1,4 +1,11 @@
 export const actions = {
-  "click": "Click",
-  "load": "Navigate"
+  click: "Click",
+  load: "Navigate",
+  keydown: "Fill Field",
+  START: 'START',
+  STOP: 'STOP',
+  CREATE_ANNOTATION: 'Apply Annotation',
+  TEST_SELECTOR: 'TEST_SELECTOR',
+  ITEM_SELECTED: 'ITEM_SELECTED',
+  ELEMENT_SCREENSHOT: 'Element Screenshot'
 }
