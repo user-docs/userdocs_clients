2341eab5447cb43c58a0baf25bc0423920aa7a33
johns10 <johns10davenport@gmail.com>
2021-06-27 13:59:54 -0400

    Add badge blur handler

--- packages/runner/src/annotation/annotation.ts
+++ packages/runner/src/annotation/annotation.ts
@@ -7,6 +7,88 @@ declare global {
 interface AnnotationHandler { [ key: string ]: Function }
 
 export const annotationHandlers: AnnotationHandler = {
+  "Badge Blur": (step: Step, elementToAnnotate: HTMLElement, style: { [key: string]: string }) => {
+    // Get vars for these elements
+    console.log("Applying Badge Annotation")
+    const xOrientation = step.annotation.xOrientation;
+    const yOrientation = step.annotation.yOrientation;
+    const size = step.annotation.size;
+    const labelText = step.annotation.label;
+    const color = step.annotation.color;
+    const xOffset = step.annotation.xOffset;
+    const yOffset = step.annotation.yOffset;
+    const fontSize = step.annotation.fontSize;
+    const annotationId = step.annotation.id;
+
+    var locatorElement = document.createElement('div');
+
+    // Append locator to parent
+    //elementToAnnotate.prepend(locatorElement);
+    elementToAnnotate.insertAdjacentElement('beforebegin', locatorElement)
+
+    locatorElement.id = `userdocs-annotation-${annotationId}-locator`
+    locatorElement.classList.add("userdocs-locator")
+    const styleLocator = new Function(`return ${style.styleLocator}`)()
+    locatorElement = styleLocator(locatorElement, elementToAnnotate)
+
+    // Position Locator
+    const parentRect = elementToAnnotate.getBoundingClientRect();
+    const elementRect = locatorElement.getBoundingClientRect();
+    const parentStyle = window.getComputedStyle(elementToAnnotate)
+    const locatorStyle = window.getComputedStyle(locatorElement)
+    const topAdjustment = parentRect.top - elementRect.top
+    const leftAdjustment = parentRect.left - elementRect.left
+    
+    console.log(`
+      Locator Top is ${elementRect.top}. 
+      Parent Top is ${parentRect.top}. 
+      Margin Top is ${parentStyle.marginTop}.  
+      Padding top is ${parseInt(parentStyle.paddingTop)}. 
+      Top adjustment of ${topAdjustment}
+    `)
+    console.log(`
+      Locator Left is ${elementRect.left}. 
+      Parent Left is ${parentRect.left}. 
+      Margin Left is ${parentStyle.marginLeft}.  
+      Padding left is ${parseInt(parentStyle.paddingLeft)}. 
+      Left adjustment of ${leftAdjustment}
+    `)
+    locatorElement.style.top = `${parseInt(locatorStyle.top) + topAdjustment}px`
+    locatorElement.style.left = `${parseInt(locatorStyle.left) + leftAdjustment}px`
+    // End Position Locator
+
+    var maskElement = document.createElement('div');
+    maskElement.id = `userdocs-mask-${annotationId}-mask`
+    maskElement.classList.add("userdocs-mask")
+    const styleMask = new Function(`return ${style.styleMask}`)()
+    maskElement = styleMask(maskElement, elementToAnnotate)
+
+    // Append mask to locator
+    locatorElement.append(maskElement);
+  
+    var badgeElement = document.createElement('span');
+    badgeElement.id = `userdocs-annotation-${annotationId}-badge`
+    badgeElement.textContent = labelText;
+    badgeElement.classList.add("userdocs-badge")
+    const styleBadge = new Function(`return ${style.styleBadge}`)()
+    badgeElement = styleBadge(badgeElement, maskElement, size, fontSize, color, xOrientation, yOrientation, xOffset, yOffset);
+
+    elementToAnnotate.style.textShadow = "rgba(0, 0, 0, 0.5) 0px 0px 5px";
+    elementToAnnotate.style.color = "transparent";
+  
+    try {
+      maskElement.append(badgeElement); 
+      badgeElement.style.zIndex = '999'
+      if (window.active_annotations) {
+        window.active_annotations.push(locatorElement);
+      } else {
+        window.active_annotations = [ locatorElement ]
+      }
+      return step
+    } catch(error) {
+      throw(error)
+    }
+  },
   "Badge": (step: Step, elementToAnnotate: HTMLElement, style: { [key: string]: string }) => {
     // Get vars for these elements
     console.log("Applying Badge Annotation")
