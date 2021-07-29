c95bbabd84af19f80485e50695de99a9ba695fa9
johns10 <johns10davenport@gmail.com>
2021-07-28 20:28:36 -0400

    revise to accept url

--- packages/desktop/src/main_window/login.ts
+++ packages/desktop/src/main_window/login.ts
@@ -1,31 +1,23 @@
 import axios from 'axios';
 
-export async function loginAPI(email, password, isDev) {
-  console.log("loginAPI")
-  let url
+export async function loginAPI(email, password, url) {
+  const sessionUrl = url + "/api/session"
 	const params = {
     'user[email]': email,
     'user[password]': password
 	}
 
-  if(isDev) url = "https://dev.user-docs.com:4002/api/session"
-  else url = "https://app.user-docs.com/api/session"
-
 	try {
-		const response = await axios.post(url, null, { params: params })
+		const response = await axios.post(sessionUrl, null, { params: params })
 		return response.data.data
 	} catch (error) {
     throw error
 	}
 }
 
-export async function loginUI(access_token, isDev) {
-  console.log("loginUI")
-  let url
-
-  if(isDev) url = "https://dev.user-docs.com:4002/api/session/convert"
-  else url = "https://app.user-docs.com/api/session/convert"
-
-  const response = await axios.post(url, null, {headers: {'authorization': access_token}})
+export async function loginUI(token, url) {
+  console.log(token)
+  let convertUrl = url + "/api/session/convert"
+  const response = await axios.post(convertUrl, null, {headers: {'authorization': token}})
   return response
 }
