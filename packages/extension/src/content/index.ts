83cbdf011ea4530a093440375d290772fcbf1617
johns10 <johns10davenport@gmail.com>
2021-08-29 00:06:02 -0400

    Comment out attaching page script

--- packages/extension/src/content/index.ts
+++ packages/extension/src/content/index.ts
@@ -42,29 +42,12 @@ chrome.storage.local.set({
   token: localStorage.token,
   wsUrl: localStorage.wsUrl,
 })
+/*
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('dist/page.js');
+s.onload = function() {
+    (this as any).remove();
+};
 
-  var s = document.createElement('script');
-  s.src = chrome.runtime.getURL('dist/page.js');
-  s.onload = function() {
-      (this as any).remove();
-  };
-  (document.head || document.documentElement).appendChild(s);
-  chrome.runtime.sendMessage({action: actions.GET_AUTH})
-}
-
-window.generateSelector = Recorder.generateSelector 
-window.addEventListener("message", (event) => {
-  // We only accept messages from ourselves
-  if (event.source != window) return;
-  if (event.data.type) {port.postMessage(event.data.text);}
-  if (event.data.action == "sendAuth") {chrome.runtime.sendMessage(event.data)}
-}, false);
-
-chrome.runtime.onMessage.addListener(msg => { 
-  let logString = "Content Script received a message."
-  if (msg.action) {
-    if (msg.action === actions.GET_AUTH) window.postMessage(msg, "*");
-  }
-})
-
-boot()
+(document.head || document.documentElement).appendChild(s);
+*/
