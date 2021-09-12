7965bff613c90a02f07d482047c36a63aa6e7e9a
johns10 <johns10davenport@gmail.com>
2021-09-12 14:53:00 -0400

    New Actions

--- packages/extension/src/actions.ts
+++ packages/extension/src/actions.ts
@@ -1,12 +1,15 @@
 export const actions = {
   click: "Click",
   load: "Navigate",
-  keydown: "Fill Field",
+  keypress: "Fill Field",
   START: 'START',
   STOP: 'STOP',
   CREATE_ANNOTATION: 'Apply Annotation',
   TEST_SELECTOR: 'TEST_SELECTOR',
   ITEM_SELECTED: 'ITEM_SELECTED',
   ELEMENT_SCREENSHOT: 'Element Screenshot',
-  GET_AUTH: 'getAuth'
+  GET_AUTH: 'getAuth',
+  SAVE_STEP: "save_step",
+  GET_CLICKED_ELEMENT: "get_clicked_element",
+  FULL_SCREEN_SCREENSHOT: "Full Screen Screenshot"
 }
