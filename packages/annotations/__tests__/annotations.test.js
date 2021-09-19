ff6ef098fa20ffc45a40a89716d90fcae218cac9
John Davenport <johns10davenport@gmail.com>
2021-09-19 11:38:07 -0400

    Tests

--- packages/annotations/__tests__/annotations.test.js
+++ packages/annotations/__tests__/annotations.test.js
@@ -4,7 +4,7 @@ const fixtures = require('../fixtures/annotations.js')
 const { applyAnnotation, badge } = require('../lib/annotations');
 
 test('applyAnnotation', () => {
-  document.body.innerHTML = '<div><p id="t">Hello</p><p id="t2">World</p></div>';
+  document.body.innerHTML = '<div><p id="t">Hello</p><p id="t2" style="top: 5px; left: 5px;">World</p></div>';
   const element = document.getElementById("t2");
   applyAnnotation(fixtures.badge, element)
   var badgeElement = document.getElementById(`userdocs-annotation-1-badge`)
@@ -12,11 +12,10 @@ test('applyAnnotation', () => {
 })
 
 test('badge', () => {
-  document.body.innerHTML = '<div><p id="t">Hello</p><p id="t2">World</p></div>';
+  document.body.innerHTML = '<div><p id="t">Hello</p><p id="t2" style="top: 5px; left: 5px;">World</p></div>';
   const element = document.getElementById("t2")
   badge(fixtures.badge, element, {})
   var badgeElement = document.getElementById(`userdocs-annotation-1-badge`)
   var locatorElement = document.getElementById(`userdocs-annotation-1-locator`)
-  console.log(locatorElement)
   expect(badgeElement.classList.contains('userdocs-badge')).toBeTruthy();
 });
