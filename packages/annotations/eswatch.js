7125bc979759ae3e080a80d73e41a780f654c803
John Davenport <johns10davenport@gmail.com>
2021-09-23 22:54:24 -0400

    watch script too

---
+++ packages/annotations/eswatch.js
@@ -0,0 +1,16 @@
+const esbuild = require('esbuild')
+
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+esbuild.build({
+  entryPoints: ['./lib/browser.ts'],
+  outfile: 'dist/bundle.js',
+  watch: {
+    onRebuild(error, result) {
+      if (error) console.error('watch build failed:', error)
+      else console.log('watch build succeeded:', result)
+    },
+  },
+  bundle: true
+}).catch(() => process.exit(1))
